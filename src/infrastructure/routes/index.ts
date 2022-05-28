cc6792562086a065774d46abcc985e02fee59519
Guilherme Melo <jguilhermefmelo21@gmail.com>
2022-05-27 23:48:15 -0300

    delete

--- src/infrastructure/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Request, Response, Router } from 'express';
-
-const routes = Router();
-
-routes.get('/', (req: Request, res: Response) => {
-  return res.json({ message: 'Larissa, guilherme quer cuzinho!' });
-});
-
-export { routes };
--- src/infrastructure/server.ts
+++
@@ -1,7 +0,0 @@
-import { app } from "./app";
-
-const PORT = 3333;
-
-app.listen(PORT, ()=>{
-  console.log(`"Server started on port ${PORT}`)
-})
